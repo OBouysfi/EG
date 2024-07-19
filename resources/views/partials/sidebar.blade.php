692ccaa59acdfd87fe3752550d1800fdb42fbab7
oussama-nassoh <101725275+oussama-nassoh@users.noreply.github.com>
2024-07-19 01:10:57 +0100

    deleteatistationribruce

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -68,24 +68,6 @@
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
-                        <i data-feather="file-text" class="feather-icon"></i>
-                        <span class="hide-menu">Attestations</span>
-                    </a>
-                    <ul aria-expanded="false" class="collapse first-level base-level-line">
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="">
-                                <span class="hide-menu">Liste des Attestations</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="">
-                                <span class="hide-menu">Ajouter une Attestation</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
                         <i data-feather="dollar-sign" class="feather-icon"></i>
                         <span class="hide-menu">Suivi Paiements</span>
                     </a>
