7642fcd7ec5b2b7f18137316850ef0cea75f3f89
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2024-07-26 12:29:13 +0200

    fixed bug affichage

--- resources/views/paiements/listing.blade.php
+++ resources/views/paiements/listing.blade.php
@@ -53,35 +53,37 @@
 <div class="page-wrapper">
     <div class="container-fluid">
         <div class="row">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h3 class="mb-0 text-dark">Liste des Paiements</h3>
-                    <div>
-                        <a href="{{ route('paiements.create') }}" class="btn btn-primary" style="background: #004F6D !important;">
-                            <i class="fa fa-plus"></i> Ajouter
-                        </a>
-                        <button class="btn btn-secondary" style="background: #003F49;">
-                            <i class="fa fa-download"></i> Télécharger
-                        </button>
-                        <button id="printButton" class="btn btn-info" style="background: #006064;">
-                            <i class="fa fa-print"></i> Imprimer
-                        </button>
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <h3 class="mb-0 text-dark">Liste des Paiements</h3>
+                        <div>
+                            <a href="{{ route('paiements.create') }}" class="btn btn-primary" style="background: #004F6D !important;">
+                                <i class="fa fa-plus"></i> Ajouter
+                            </a>
+                            <button class="btn btn-secondary" style="background: #003F49;">
+                                <i class="fa fa-download"></i> Télécharger
+                            </button>
+                            <button id="printButton" class="btn btn-info" style="background: #006064;">
+                                <i class="fa fa-print"></i> Imprimer
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table id="paiements-table" class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Participant</th>
-                                    <th scope="col">Séance</th>
-                                    <th scope="col">Montant</th>
-                                    <th scope="col">Date de Paiement</th>
-                                    <th scope="col">Actions</th>
-                                </tr>
-                            </thead>
-                        </table>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="paiements-table" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Participant</th>
+                                        <th scope="col">Séance</th>
+                                        <th scope="col">Montant</th>
+                                        <th scope="col">Date de Paiement</th>
+                                        <th scope="col">Actions</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
